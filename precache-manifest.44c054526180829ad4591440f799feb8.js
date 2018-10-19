f795b55bbd94fe1dc594d3efc504d4569d5bbe50
Dev Nguyen <devnguyen@Rigby-20.local>
2018-10-18 21:07:08 -0500

    Updates

---
+++ precache-manifest.44c054526180829ad4591440f799feb8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "62036c4b8e85dfd2ff13",
+    "url": "/random-office-quote-generator/static/js/runtime~main.62036c4b.js"
+  },
+  {
+    "revision": "269407ee63772b9e95a0",
+    "url": "/random-office-quote-generator/static/js/main.269407ee.chunk.js"
+  },
+  {
+    "revision": "ccf717e8f1c052517a1a",
+    "url": "/random-office-quote-generator/static/js/1.ccf717e8.chunk.js"
+  },
+  {
+    "revision": "269407ee63772b9e95a0",
+    "url": "/random-office-quote-generator/static/css/main.d56fe341.chunk.css"
+  },
+  {
+    "revision": "a709e18ecdb503018799d2874c005643",
+    "url": "/random-office-quote-generator/index.html"
+  }
+];
